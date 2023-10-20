0ef57de61899aa7b1e724ad47ba44c970d139abe
Arman Mazdaee <ar.mazdaee@gmail.com>
2023-10-21 02:23:23 +0330

    add AddressSet and change moderator implementions

--- smart-contracts/contracts/Topics.sol
+++ smart-contracts/contracts/Topics.sol
@@ -2,10 +2,13 @@
 pragma solidity ^0.8.20;
 
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {AddressSet, AddressSetLib} from "./libraries/AddressSet.sol";
 import {IRefiqTopics} from "./interfaces/ITopics.sol";
 
+using AddressSetLib for AddressSet;
+
 contract RefiqTopics is ERC721, IRefiqTopics {
-    mapping(uint256 topicId => mapping(address user => bool isModerator))
+    mapping(uint256 topicId => AddressSet moderatorAddresses)
         private _moderators;
     mapping(uint256 contentId => uint256 topicId) private _topics;
     mapping(uint256 contentId => address author) private _authors;
@@ -26,24 +29,18 @@ contract RefiqTopics is ERC721, IRefiqTopics {
         if (msg.sender != ownerOf(topicId)) {
             revert Unauthorized();
         }
-        bool isModerator = _moderators[topicId][moderator];
-        if (isModerator) {
-            return;
+        if (_moderators[topicId].insert(moderator)) {
+            emit ModeratorAdded(topicId, moderator);
         }
-        _moderators[topicId][moderator] = true;
-        emit ModeratorAdded(topicId, moderator);
     }
 
     function removeModerator(uint256 topicId, address moderator) external {
         if (msg.sender != ownerOf(topicId)) {
             revert Unauthorized();
         }
-        bool isModerator = _moderators[topicId][moderator];
-        if (!isModerator) {
-            return;
+        if (_moderators[topicId].remove(moderator)) {
+            emit ModeratorRemoved(topicId, moderator);
         }
-        delete _moderators[topicId][moderator];
-        emit ModeratorRemoved(topicId, moderator);
     }
 
     function updateTopicInfo(
@@ -51,7 +48,9 @@ contract RefiqTopics is ERC721, IRefiqTopics {
         string calldata infoCid
     ) external {
         address sender = msg.sender;
-        if (!_moderators[topicId][sender] && sender != ownerOf(topicId)) {
+        if (
+            !_moderators[topicId].contains(sender) && sender != ownerOf(topicId)
+        ) {
             revert Unauthorized();
         }
         emit TopicInfoUpdated(topicId, infoCid);
@@ -91,7 +90,7 @@ contract RefiqTopics is ERC721, IRefiqTopics {
         address sender = msg.sender;
         if (
             _authors[contentId] != sender &&
-            !_moderators[topicId][sender] &&
+            !_moderators[topicId].contains(sender) &&
             sender != ownerOf(topicId)
         ) {
             revert Unauthorized();
